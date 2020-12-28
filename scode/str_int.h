c2ffa3b61b96f310618654c8065a65a83207aa4d
emperorlu <emperorlu@hust.edu.cn>
2020-12-28 15:46:21 +0800

    [Debug] Not Fount

--- scode/str_int.h
+++ scode/str_int.h
@@ -13,7 +13,6 @@ using  namespace std;
 //vector<int> Code::toCode(vector<string> keys){
 
 vector<int> toCode(vector<string> keys){
-    //vector<char*> tmp;
     vector<char> based_char;
     vector<int> based;
     int pre_ba;
@@ -22,23 +21,17 @@ vector<int> toCode(vector<string> keys){
     vector<int> turn;
     int max_lenth = 0;
     for (int i = 0; i < keys.size(); i++){
-        //strrev(keys[i]);
         reverse(keys[i].begin(),keys[i].end());
         if (keys[i].length() > max_lenth) max_lenth = keys[i].length();
-        //strcpy(tmp[i],keys[i].c_str());
-
     }
     for (int i = 0; i < max_lenth; i++){
-    //for (int i = max_lenth-1; i >= 0; i--){
         vector<char> compare;
         for (int j = 0; j < keys.size(); j++) {
-            cout << i << ": " << j << ": " << keys[j][i] << endl;
             compare.push_back(keys[j][i]);
         }
         vector<char>::iterator max = max_element(compare.begin(), compare.end());
         vector<char>::iterator min = min_element(compare.begin(), compare.end());
         based_char.push_back(*min);
-        cout << i << ": max;min;pre_ba " << *max <<  "; " << *min <<  "; " << pre_ba << endl;
         if (i != 0) ba *= pre_ba;
         pre_ba = *max-*min+1;
         based.push_back(ba);
@@ -47,14 +40,9 @@ vector<int> toCode(vector<string> keys){
         int num = 0;
         for (int j = 0; j < keys[i].length(); j++){
             num += based[j] * (int)(keys[i][j] - based_char[j]);
-            cout << i << ": " << j << ":  num+ " << num << endl;
-            cout << keys[i][j] << " ; " << based_char[j] << endl;
-            cout << based[j] << " ; " <<  (int)(keys[i][j] - based_char[j]) << endl;
         }
         
         turn.push_back(num);
     }
-    for (int i = 0; i < based.size(); i++)
-        cout << i << "_based: " << based[i] << endl; 
     return turn;
 }
