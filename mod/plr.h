c7c98012238ba55b4825ea193a55112db4df84b3
emperorlu <emperorlu@hust.edu.cn>
2020-12-29 16:37:31 +0800

    [Debug] Add mod code

--- mod/plr.h
+++ mod/plr.h
@@ -20,7 +20,7 @@ struct line {
 class Segment {
 public:
     Segment(uint64_t _x, double _k, double _b, uint64_t _x2) : x(_x), k(_k), b(_b), x2(_x2) {}
-    uint64_t x;
+    double x;
     double k;
     double b;
     uint64_t x2;
