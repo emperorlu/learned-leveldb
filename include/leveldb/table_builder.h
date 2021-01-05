2730c4b937b31ee2baf370117568567e8bdb0ab6
emperorlu <emperorlu@hust.edu.cn>
2021-01-05 14:31:21 +0800

    [Debug] Add mod code

--- include/leveldb/table_builder.h
+++ include/leveldb/table_builder.h
@@ -14,11 +14,12 @@
 #define STORAGE_LEVELDB_INCLUDE_TABLE_BUILDER_H_
 
 #include <stdint.h>
-
+#include "mod/learned_index.h"
 #include "leveldb/export.h"
 #include "leveldb/options.h"
 #include "leveldb/status.h"
 
+using adgMod::LearnedIndexData;
 namespace leveldb {
 
 class BlockBuilder;
@@ -34,7 +35,7 @@ class LEVELDB_EXPORT TableBuilder {
 
   TableBuilder(const TableBuilder&) = delete;
   TableBuilder& operator=(const TableBuilder&) = delete;
-  adgMod::LearnedIndexData* LearnedMod;
+  LearnedIndexData* LearnedMod;
   // REQUIRES: Either Finish() or Abandon() has been called.
   ~TableBuilder();
 
